45090ca9bd9d2c364423697f9b1ab7f3dcfd23b5
Park Geon Woo <ayyymbebo@gmail.com>
2020-05-11 03:35:23 +0900

    #1.14 User Entity part Two

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from "typeorm";
 const connectionOptions: ConnectionOptions = {
   type: "postgres",
   database: "nuber",
-  synchronize: true,
+  synchronize: true, //서버가 실행 될 때마다, DB를 원하는 모양으로 동기화
   logging: true,
   entities: ["entities/**/*.*"],
   host: process.env.DB_ENDPOINT,
