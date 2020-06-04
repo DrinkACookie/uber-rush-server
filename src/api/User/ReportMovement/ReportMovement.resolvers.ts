6c66ef7159c84884e14a26bf151ed500416559b4
Park Geon Woo <ayyymbebo@gmail.com>
2020-06-05 02:01:23 +0900

    #1.59 ReportMovement Resolver

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,21 +1,20 @@
 import { Resolvers } from "../../../types/resolver";
 import {
-  UpdateMyProfileResponse,
-  UpdateMyProfileMutationArgs,
+  ReportMovementResponse,
+  ReportMovementMutationArgs,
 } from "../../../types/graph";
 import privateResolver from "../../../utils/privateResolver";
 import User from "../../../entities/User";
-import { userInfo } from "os";
 import cleanNullArgs from "../../../utils/cleanNullArgs";
 
 const resolvers: Resolvers = {
   Mutation: {
-    UpdateMyProfile: privateResolver(
+    ReportMovement: privateResolver(
       async (
         _,
-        args: UpdateMyProfileMutationArgs,
+        args: ReportMovementMutationArgs,
         { req }
-      ): Promise<UpdateMyProfileResponse> => {
+      ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
         try {
