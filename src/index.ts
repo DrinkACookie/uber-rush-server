64cc8bcd4711b3cb902a3be08d56f9c3c1ccdb7d
Park Geon Woo <ayyymbebo@gmail.com>
2020-07-15 21:16:02 +0900

    #2.0 Trouble shooting about Connecttion & Login

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,12 @@ const appOptions: Options = {
     },
   },
 };
-
 const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
-createConnection(connectionOptions).then(() => {
-  app.start(appOptions, handleAppStart);
-});
+createConnection(connectionOptions)
+  .then(() => {
+    app.start(appOptions, handleAppStart);
+  })
+  .catch((Error) => {
+    console.log("error in createConnection : ", Error);
+  });
