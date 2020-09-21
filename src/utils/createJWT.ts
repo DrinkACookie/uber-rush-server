02db3a6225dfadaecaa314eebcea774b667dd6f6
ParkGeonWoo <ayyymbebo@gmail.com>
2020-09-21 22:59:59 +0900

    jwt  만료일 설정

--- src/utils/createJWT.ts
+++ src/utils/createJWT.ts
@@ -2,8 +2,9 @@ import { sign } from "jsonwebtoken";
 
 const createJWT = (id: number): string => {
         const token = sign(
-                { id }, process.env.JWT_TOKEN||""
-        );
+                { id }, process.env.JWT_TOKEN||""// option : https://www.npmjs.com/package/jsonwebtoken 
+                ,{expiresIn:"2 days"}
+                );
         return token;
 }
 
